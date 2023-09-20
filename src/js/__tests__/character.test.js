1df05979578ba497429b9a6d6a6e0488227f55ab
Konstantin Zatyura <19kostik94@mail.ru>
2023-09-21 01:30:52 +0700

    v2

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,14 +1,49 @@
 import Character from '../character.js';
 import Bowman from '../bowman.js';
+import Magician from '../magician.js';
 
 test('short name', () => {
     expect(() => new Character('A', 'Bowman')).toThrow('Invalid name length');
   });
   
-  test('long name', () => {
+test('long name', () => {
     expect(() => new Character('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'Bowman')).toThrow('Invalid name length');
   });
   
-  test('ivalid character type', () => {
+test('ivalid character type', () => {
     expect(() => new Character('Nick', 'Warrior')).toThrow('Invalid character type');
   });
+
+test('levelup function on dead  Magician', () => {
+    const result = new Character('Nick', 'Magician');
+    result.health = 0;
+    expect(() => {
+      result.levelUp();
+    }).toThrow('This character is dead');
+  });
+
+test('levelup function on Bowman', () => {
+    const result = new Bowman('Nick', 'Bowman');
+    result.levelUp();
+    expect(result).toEqual({
+      name: 'Nick',
+      type: 'Bowman',
+      health: 100,
+      level: 2,
+      attack: 30,
+      defence: 30
+    });
+  });
+  
+test('function damage on character Magician', () => {
+    const result = new Magician('Nick', 'Magician');
+    result.damage(20);
+    expect(result).toEqual({
+      name: 'Nick',
+      type: 'Magician',
+      health: 88,
+      level: 1,
+      attack: 10,
+      defence: 40,
+    });
+});
--- src/js/character.js
+++ src/js/character.js
@@ -21,6 +21,23 @@ export default class Character {
       this.health = 100;
       this.level = 1;
     }
+
+    levelUp(){
+      if (this.health !==0) {
+        this.level ++;
+        this.attack *= 1.2;
+        this.defence *= 1.2;
+        this.health = 100;
+      } else {
+        throw new Error('This character is dead');
+      }
+    }
+
+    damage(points) {
+      if (this.health > 0) {
+        this.health -= points * (1 - this.defence / 100);
+      }
+    }
 }
 
 
